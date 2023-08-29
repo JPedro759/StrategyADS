c3446eafb33988088dc59376f03eb5ef25e7d809
João Pedro <77515431+JPedro759@users.noreply.github.com>
2023-08-29 19:19:15 -0300

    Update strategy.cs

--- strategy.cs
+++ strategy.cs
@@ -1,64 +1,96 @@
-using System;
-
 public class HelloWorld {
-    public static void Main(string[] args) {
-        Arithmetic calculation = new(); 
+    public static void main(String[] args) {
+        ICalcAverage calculation = new Arithmetic();
         
-        Discipline discipline = new(calculation);
-        discipline.P1 = 10;
-        discipline.P2 = 5;
-	discipline.CalculateAverage();
+        Discipline discipline = new Discipline(calculation);
+        discipline.setP1(10);
+        discipline.setP2(5);
+        discipline.calculateAverage();
         
-	Console.WriteLine($"P1: {discipline.P1:F2}; P2: {discipline.P2:F2}; Média: {discipline.Average:F2}; Situação: {discipline.Situation}");
+        System.out.printf("P1: %.2f; P2: %.2f; Média: %.2f; Situação: %s%n", discipline.getP1(), discipline.getP2(), discipline.getAverage(), discipline.getSituation());
     }
 }
 
 interface ICalcAverage {
-    public double calcAverage(double p1, double p2);
-    public string situation(double average);
+    double calcAverage(double p1, double p2);
+    String situation(double average);
 }
 
-class Arithmetic : ICalcAverage {
+class Arithmetic implements ICalcAverage {
     public double calcAverage(double p1, double p2) {
         return (p1 + p2) / 2;
     }
 
-    public string situation(double average) {
+    public String situation(double average) {
         return average > 5.0 ? "Aprovado!" : "Reprovado!";
     }
 }
 
-class Geometric : ICalcAverage {
+class Geometric implements ICalcAverage {
     public double calcAverage(double p1, double p2) { 
-        return Math.Sqrt(p1 * p2); 
+        return Math.sqrt(p1 * p2); 
     }
 
-    public string situation(double average) { 
+    public String situation(double average) { 
         return average > 7.0 ? "Aprovado!" : "Reprovado!"; 
     }
 }
 
 class Discipline {
     private ICalcAverage _calcAverage;
-    private string _name;
+    private String _name;
     private double _p1;
     private double _p2;
     private double _average;
-    private string _situation;
+    private String _situation;
     
-    public ICalcAverage CalcAverage { get => _calcAverage; set => _calcAverage = value; }
-    public double Average { get => _average; set => _average = value; }
-    public string Name { get => _name; set => _name = value; }
-    public double P1 { get => _p1; set => _p1 = value; }
-    public double P2 { get => _p2; set => _p2 = value; }
-    public string Situation { get => _situation; set => _situation = value; }
-	
-    public void CalculateAverage() {
-	Average = _calcAverage.calcAverage(P1, P2);
-	Situation = _calcAverage.situation(Average);
+    public ICalcAverage getCalcAverage() {
+        return _calcAverage;
+    }
+    public void setCalcAverage(ICalcAverage calcAverage) {
+        this._calcAverage = calcAverage;
     }
     
-    public Discipline(ICalcAverage average) {
-        _calcAverage = average;
+    public double getAverage() {
+        return _average;
+    }
+    public void setAverage(double average) {
+        this._average = average;
+    }
+    
+    public String getName() {
+        return _name;
+    }
+    public void setName(String name) {
+        this._name = name;
+    }
+    
+    public double getP1() {
+        return _p1;
+    }
+    public void setP1(double p1) {
+        this._p1 = p1;
+    }
+    
+    public double getP2() {
+        return _p2;
+    }
+    public void setP2(double p2) {
+        this._p2 = p2;
+    }
+    
+    public String getSituation() {
+        return _situation;
+    }
+    public void setSituation(String situation) {
+        this._situation = situation;
+    }
+	
+    public void calculateAverage() {
+        _average = _calcAverage.calcAverage(_p1, _p2);
+        _situation = _calcAverage.situation(_average);
+    }
+    public Discipline(ICalcAverage averageCalculator) {
+        _calcAverage = averageCalculator;
     }
 }
